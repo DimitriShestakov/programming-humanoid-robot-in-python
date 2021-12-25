6a66f5dd068b57dfab11def3811fd8498eef2bbf
Dima <60180483+DimitriShestakov@users.noreply.github.com>
2021-12-25 13:47:31 +0100

    Add files via upload

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -6,11 +6,21 @@
  * Hints: [threading](https://docs.python.org/2/library/threading.html) may be needed for monitoring if the task is done
 '''
 
+import numpy as np
+import sys
+import os
+from keyframes import *
 import weakref
+import threading
+import xmlrpclib
+sys.path.append(os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), '..', 'joint_control'))
+
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
     '''
+
     def __init__(self, obj):
         self.proxy = weakref.proxy(obj)
 
@@ -18,30 +28,46 @@ class PostHandler(object):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
 
+        thread_execute_keyframes = threading.Thread(
+            target=self.proxy.execute_keyframes, args=[keyframes])
+        thread_execute_keyframes.start()
+
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
+
+        thread_set_transform = threading.Thread(
+            target=self.proxy.set_transform, args=[effector_name, transform])
+        thread_set_transform.start()
 
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
     '''
     # YOUR CODE HERE
+
     def __init__(self):
         self.post = PostHandler(self)
-    
+        self.server = xmlrpclib.ServerProxy('http://localhost:8000')
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
-    
+
+        angle = self.server.get_angle(joint_name)
+        print(angle)
+        return angle
+
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        return self.server.set_angle(joint_name, str(angle))
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.server.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
@@ -49,18 +75,25 @@ class ClientAgent(object):
         '''
         # YOUR CODE HERE
 
+        return self.server.execute_keyframes(keyframes)
+
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+
+        return self.server.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
 
+        return self.server.set_transform(effector_name, transform)
+
+
 if __name__ == '__main__':
     agent = ClientAgent()
     # TEST CODE HERE
-
-
+    print(agent.get_transform('HeadYaw'))
+    agent.set_angle('HeadYaw', 1)
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -12,30 +12,46 @@
 '''
 
 # add PYTHONPATH
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+from recognize_posture import PostureRecognitionAgent
+from inverse_kinematics import InverseKinematicsAgent
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
+import numpy as np
+import pickle
+sys.path.append(os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), '..', 'kinematics'))
 
-from inverse_kinematics import InverseKinematicsAgent
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
 
 
 class ServerAgent(InverseKinematicsAgent):
-    '''ServerAgent provides RPC service
+    '''ServerAgent provides RPC servic
     '''
     # YOUR CODE HERE
-    
+
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
-    
+
+        return self.perception.joint[joint_name]
+
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
 
+        self.target_joints[joint_name] = angle
+
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return pickle.dumps(self.recognize_posture(self.perception))
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
@@ -43,17 +59,29 @@ class ServerAgent(InverseKinematicsAgent):
         '''
         # YOUR CODE HERE
 
+        self.keyframes = pickle.loads(keyframes)
+
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+
+        return pickle.dumps(self.local_trans(name, self.perception.joint[name]))
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
 
+        self.set_transforms(effector_name, pickle.loads(transform))
+
+
+server = SimpleXMLRPCServer(('localhost', 9000))
+server.register_introspection_functions()
+server.register_instance(ServerAgent())
+server.serve_forever()
+
+
 if __name__ == '__main__':
     agent = ServerAgent()
     agent.run()
-
